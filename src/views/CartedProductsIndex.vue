2e51a34a4934bef8e66cea384b567dba2e000095
Brandy McKinney <missearlg80@gmail.com>
2021-07-08 21:14:36 -0400

    added a randomizer button

--- src/views/CartedProductsIndex.vue
+++ src/views/CartedProductsIndex.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="cartedproductsindex">
     <h1>Your Saved Charities</h1>
-    <button v-on:click="selectCharitys()">Randomly Select Charity</button>
+    <button v-on:click="randomizedCharitys()">Randomly Select Charity</button>
 
     <div v-for="cartedproduct in cartedproducts" v-bind:key="cartedproduct.id">
       <!-- <h2>{{ cartedproduct.charity }}</h2> -->
@@ -12,7 +12,8 @@
       <h2>I give to this charity {{ cartedproduct.frequency }}.</h2>
       <h2>This charity receives $ {{ cartedproduct.amount }}.</h2>
       <h2>Started giving on {{ cartedproduct.start_date }}.</h2>
-      <button v-on:click="selectCharitys()">Donate</button>
+      <button v-on:click="donateButton()">Donate</button>
+      <!-- <button v-on:click="destroyCartedProduct(cartedProduct)">Remove Charity</button> -->
 
       <!-- <img v-bind:src="charity.url" v-bind:alt="charity.name" /> -->
     </div>
@@ -25,6 +26,7 @@ export default {
   data: function () {
     return {
       cartedproducts: [],
+      chosenCharity: "",
     };
   },
   created: function () {
@@ -38,8 +40,24 @@ export default {
         // this.$router.push("/newcartedproduct");
       });
     },
+    // destroyCartedProduct: function (cartedProduct) {
+    //   console.log("destroy recipe", cartedProduct);
+    //   axios.delete("http://localhost:3000/carted_products/" + cartedProduct.id).then((response) => {
+    //     console.log("Destroy success", response.data);
+    //     var index = this.cartedproduct.indexOf(cartedProduct);
+    //     this.cartedProduct.splice(index, 1);
+    //   });
+    // },
+
+    randomizedCharitys: function () {
+      var chosenNumber = Math.floor(Math.random() * this.cartedproducts.length);
+      this.chosenCharity = this.cartedproducts[chosenNumber];
+      console.log("this button works", this.chosenCharity);
+    },
   },
 };
 </script>
 
-// add update and destroy to saved charities (update with date etc, and option to delete)
+// add update and destroy to saved charities (update with date etc, and option to delete) // if condition to start
+adding charities if the user has a blank saved charity list, route to list of all charities // Next page to prevent
+endless scroll 10 per page
