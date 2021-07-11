acfbea39b69eb69fe7e3290c37ac18b9a5dd0873
Brandy McKinney <missearlg80@gmail.com>
2021-07-11 18:44:59 -0400

    added selection boxes

--- src/views/CartedProductsIndex.vue
+++ src/views/CartedProductsIndex.vue
@@ -2,23 +2,38 @@
   <div section id="saved" class="cartedproductsindex">
     <h1>Your Saved Charities</h1>
     <button v-on:click="randomizedCharitys()">Randomly Select Charity</button>
-    <div v-for="cartedproduct in cartedproducts" v-bind:key="cartedproduct.id">
-      <h2>{{ cartedproduct.charity.name }}</h2>
-      <h2>{{ cartedproduct.charity.url }}</h2>
-      <h2>{{ cartedproduct.charity.address }}</h2>
-      <h2>{{ cartedproduct.charity.phone_number }}</h2>
-      <h2>
-        I donate to this charity {{ cartedproduct.frequency }}.
-        <input type="text" v-model="cartedproduct.frequency" />
-      </h2>
-      <h2>
-        This charity receives $ {{ cartedproduct.amount }}.
-        <input type="integer" v-model="cartedproduct.amount" />
-      </h2>
-      <h2>Started giving on {{ cartedproduct.start_date }}.</h2>
-      <button v-on:click="confirmNotify()">Donate</button>
-      <button v-on:click="updateCartedProduct()">Edit Charity</button>
-      <button v-on:click="destroyCartedProduct(cartedProduct)">Remove Charity</button>
+    <div>
+      <div v-for="cartedproduct in cartedproducts" v-bind:key="cartedproduct.id">
+        <h2>Charity: {{ cartedproduct.charity.name }}</h2>
+        <h2>Website: {{ cartedproduct.charity.url }}</h2>
+        <h2>Address:{{ cartedproduct.charity.address }}</h2>
+        <h2>Phone Number: {{ cartedproduct.charity.phone_number }}</h2>
+        I donate to this charity:
+        <br />
+        <input type="radio" id="monthly" value="Monthly" v-model="cartedproduct.frequency" />
+        <label for="monthly">Monthly</label>
+        <br />
+        <input type="radio" id="quarterly" value="Quarterly" v-model="cartedproduct.frequency" />
+        <label for="quarterly">Quarterly</label>
+        <br />
+        <input type="radio" id="annually" value="Annually" v-model="cartedproduct.frequency" />
+        <label for="annually">Annually</label>
+        <br />
+        <span>Donation Frequency: {{ cartedproduct.frequency }}</span>
+        <!-- <h2>
+          I donate to this charity:
+          <input type="text" v-model="cartedproduct.frequency" />
+        </h2> -->
+        <h2>
+          This charity receives:
+          <input type="integer" v-model="cartedproduct.amount" />
+        </h2>
+
+        <h2>Started giving on {{ cartedproduct.start_date }}.</h2>
+        <button v-on:click="confirmNotify()">Donate</button>
+        <button v-on:click="updateCartedProduct(cartedproduct)">Edit Charity</button>
+        <button v-on:click="destroyCartedProduct(cartedProduct)">Remove Charity</button>
+      </div>
     </div>
     <li>
       <a href="/charitysindex" class="button">Add More Charities</a>
@@ -78,10 +93,10 @@ export default {
     updateCartedProduct: function (cartedproduct) {
       var params = {
         frequency: cartedproduct.frequency,
-        amount: cartedproduct.amount,
+        amount: cartedproduct.charity.amount,
       };
       axios
-        .patch("/carted_products" + cartedproduct.id, params)
+        .patch("/carted_products" + cartedproduct.charity.id, params)
         .then((response) => {
           console.log("carted products update", response);
           this.cartedproducts = {};
